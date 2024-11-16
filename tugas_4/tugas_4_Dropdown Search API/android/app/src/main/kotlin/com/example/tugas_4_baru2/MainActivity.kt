8a637caad571ffb4b791ee30b28c2da5ad5e3134
AbiyanNur <nurabiyan99@gmail.com>
2024-11-16 21:42:43 +0700

    Tugas_4 Package Flutter

---
+++ tugas_4/tugas_4_Dropdown Search API/android/app/src/main/kotlin/com/example/tugas_4_baru2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tugas_4_baru2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ tugas_4/tugas_4_Dropdown Search/android/app/src/main/kotlin/com/example/tugas_4_baru/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tugas_4_baru
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
